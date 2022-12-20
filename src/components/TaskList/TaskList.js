58f266071592ee85539d246d780c70630571b415
maksm <rayhatr3d@yandex.ru>
2022-12-20 12:45:05 +0300

    1.5.5

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,62 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import Task from '../Task/Task';
 import './TaskList.css';
 
 const TaskList = (props) => {
+  const filterFunc = function () {
+    if (props.filterStatus === 'Active') {
+      return props.activeTask.map((task) => {
+        const { id, ...taskProps } = task;
 
-    const filterFunc = function() {
-        if (props.filterStatus === 'Active') {
-            return props.activeTask.map((task) => {
-                const {id, ...taskProps} = task;
+        return (
+          <li key={id}>
+            <Task {...taskProps} onDelete={() => props.onDelete(id)} doneTask={() => props.doneTask(id)} />
+          </li>
+        )
+      });
+    } else if (props.filterStatus === 'All') {
+      return props.todos.map((task) => {
+        const { id, ...taskProps } = task;
 
-                return (
-                    <li key={id}>
-                        <Task {...taskProps}
-                              onDelete={() => props.onDelete(id)}
-                              doneTask={() => props.doneTask(id)}
-                        />
-                    </li>
-                );
+        return (
+          <li key={id}>
+            <Task {...taskProps} onDelete={() => props.onDelete(id)} doneTask={() => props.doneTask(id)} />
+          </li>
+        )
+      });
+    } else {
+      return props.comletedTask.map((task) => {
+        const { id, ...taskProps } = task;
 
-            });
-        } else if (props.filterStatus === 'All') {
-            return props.todos.map((task) => {
-                const {id, ...taskProps} = task;
-
-                return (
-                    <li key={id}>
-                        <Task {...taskProps}
-                              onDelete={() => props.onDelete(id)}
-                              doneTask={() => props.doneTask(id)}
-                        />
-                    </li>
-                );
-
-            });
-        } else {
-            return props.comletedTask.map((task) => {
-                const {id, ...taskProps} = task;
-
-                return (
-                    <li key={id}>
-                        <Task {...taskProps}
-                              onDelete={() => props.onDelete(id)}
-                              doneTask={() => props.doneTask(id)}
-                        />
-                    </li>
-                );
-
-            });
-        }
+        return (
+          <li key={id}>
+            <Task {...taskProps} onDelete={() => props.onDelete(id)} doneTask={() => props.doneTask(id)} />
+          </li>
+        )
+      });
     }
+  };
 
-
-    return (
-        <ul className="todo-list">
-            {filterFunc()}
-        </ul>
-    );
-
+  return <ul className="todo-list">{filterFunc()}</ul>;
 };
 
 export default TaskList;
--- src/components/TasksFilter/TasksFilter.js
+++ src/components/TasksFilter/TasksFilter.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import './TasksFilter.css'
-const TasksFilter = ({filterActiveTask, filterAllTask, filterCompletedTask, tabStatus}) => {
+const TasksFilter = ({ filterActiveTask, filterAllTask, filterCompletedTask, tabStatus }) => {
+  let allBtn = ''
+  let activeBtn = '';
+  let completedBtn = '';
+  if (tabStatus === 'All') {
+    allBtn = 'selected';
+  } else if (tabStatus === 'Active') {
+    activeBtn = 'selected';
+  } else if (tabStatus === 'Completed') {
+    completedBtn = 'selected';
+  }
 
-    let allBtn = ''
-    let activeBtn = ''
-    let completedBtn = ''
-    if (tabStatus === 'All') {
-        allBtn = 'selected'
-    } else if (tabStatus === 'Active') {
-        activeBtn = 'selected'
-    } else if (tabStatus === 'Completed') {
-        completedBtn = 'selected'
-    }
-
-    return (
-        <ul className="filters">
-            <li>
-                <button className={allBtn} onClick={filterAllTask}>All</button>
-            </li>
-            <li>
-                <button className={activeBtn} onClick={filterActiveTask}>Active</button>
-            </li>
-            <li>
-                <button className={completedBtn} onClick={filterCompletedTask}>Completed</button>
-            </li>
-        </ul>
-    )
+  return (
+    <ul className="filters">
+      <li>
+        <button className={allBtn} onClick={filterAllTask}>
+          All
+        </button>
+      </li>
+      <li>
+        <button className={activeBtn} onClick={filterActiveTask}>
+          Active
+        </button>
+      </li>
+      <li>
+        <button className={completedBtn} onClick={filterCompletedTask}>
+          Completed
+        </button>
+      </li>
+    </ul>
+  );
 }
 
 export default TasksFilter
